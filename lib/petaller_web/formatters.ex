600e0f3d948416f2c20a56775f41250b311c04eb
Nicolas Knoebber <knoebber@gmail.com>
2022-03-15 22:34:42 -0800

    Move formatters to module

--- lib/petaller_web/formatters.ex
+++ lib/petaller_web/formatters.ex
@@ -1,4 +1,19 @@
 defmodule PetallerWeb.Formatters do
+  def format_pace(miles, duration_in_seconds) do
+    seconds_per_mile = floor(duration_in_seconds / miles)
+    minutes_per_mile = div(seconds_per_mile, 60)
+    minute_seconds_per_mile = rem(seconds_per_mile, 60)
+    format_duration(0, minutes_per_mile, minute_seconds_per_mile)
+  end
+
+  def format_duration(hours, minutes, seconds) do
+    Enum.map(
+      [hours, minutes, seconds],
+      fn n -> Integer.to_string(n) |> String.pad_leading(2, "0") end
+    )
+    |> Enum.join(":")
+  end
+
   def format_date(naive_dt) do
     DateTime.from_naive!(naive_dt, "Etc/UTC")
     |> DateTime.shift_zone!("America/Anchorage")
