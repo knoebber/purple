2d436a3d13e27e5ced75e3c1005fd9752691e406
Nicolas Knoebber <knoebber@gmail.com>
2022-04-10 15:53:16 -0800

    Add file controller

---
+++ lib/petaller_web/controllers/file_controller.ex
@@ -0,0 +1,33 @@
+defmodule PetallerWeb.FileController do
+  use PetallerWeb, :controller
+
+  alias Petaller.Uploads
+
+  defp not_found(conn) do
+    send_resp(conn, 404, "File ref not found")
+  end
+
+  defp get_file_path(id) do
+    with {:ok, file_ref} <- Uploads.get_file_ref(id) do
+      {:ok, Uploads.get_full_upload_path(file_ref)}
+    else
+      _ -> :error
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    with {:ok, path} <- get_file_path(id) do
+      Plug.Conn.send_file(conn, 200, path)
+    else
+      _ -> not_found(conn)
+    end
+  end
+
+  def download(conn, %{"id" => id}) do
+    with {:ok, path} <- get_file_path(id) do
+      send_download(conn, {:file, path})
+    else
+      _ -> not_found(conn)
+    end
+  end
+end
