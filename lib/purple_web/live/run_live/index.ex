0cc44fa05c4e6092a928ac7f664261f761dbdee1
Nicolas Knoebber <knoebber@gmail.com>
2022-06-06 08:56:23 -0400

    Use table component in run index page

--- lib/purple_web/live/run_live/index.ex
+++ lib/purple_web/live/run_live/index.ex
@@ -122,47 +122,31 @@ defmodule PurpleWeb.RunLive.Index do
       <%= text_input(f, :query, placeholder: "Search...", phx_debounce: "200") %>
       <%= select(f, :tag, @tag_options) %>
     </.form>
-    <table class="window mt-2">
-      <thead class="bg-purple-300">
-        <tr>
-          <th>Miles</th>
-          <th>Duration</th>
-          <th>Pace</th>
-          <th>Date</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody id="runs">
-        <%= for run <- @runs do %>
-          <tr id={"run-#{run.id}"}>
-            <td>
-              <%= live_redirect(run.miles, to: Routes.run_show_path(@socket, :show, run)) %>
-            </td>
-            <td>
-              <%= format_duration(run.hours, run.minutes, run.minute_seconds) %>
-            </td>
-            <td>
-              <%= format_pace(run.miles, run.seconds) %>
-            </td>
-            <td>
-              <%= format_date(run.date, :dayname) %>
-            </td>
-            <td>
-              <%= live_patch("Edit", to: index_path(@params, :edit, run.id)) %>
-            </td>
-            <td>
-              <%= link("Delete",
-                phx_click: "delete",
-                phx_value_id: run.id,
-                data: [confirm: "Are you sure?"],
-                to: "#"
-              ) %>
-            </td>
-          </tr>
-        <% end %>
-      </tbody>
-    </table>
+    <.table rows={@runs}>
+      <:col let={run} label="Miles">
+        <%= live_redirect(run.miles, to: Routes.run_show_path(@socket, :show, run)) %>
+      </:col>
+      <:col let={run} label="Duration">
+        <%= format_duration(run.hours, run.minutes, run.minute_seconds) %>
+      </:col>
+      <:col let={run} label="Pace">
+        <%= format_pace(run.miles, run.seconds) %>
+      </:col>
+      <:col let={run} label="Date">
+        <%= format_date(run.date, :dayname) %>
+      </:col>
+      <:col let={run} label="">
+        <%= live_patch("Edit", to: index_path(@params, :edit, run.id)) %>
+      </:col>
+      <:col let={run} label="">
+        <%= link("Delete",
+          phx_click: "delete",
+          phx_value_id: run.id,
+          data: [confirm: "Are you sure?"],
+          to: "#"
+        ) %>
+      </:col>
+    </.table>
     """
   end
 end
