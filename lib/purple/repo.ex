6fa51a9b8ed755593e1bf6cf1d71f46dfceb93f1
Nicolas Knoebber <knoebber@gmail.com>
2022-09-10 21:21:01 -0700

    Add function to repo for pagination

--- lib/purple/repo.ex
+++ lib/purple/repo.ex
@@ -1,5 +1,14 @@
 defmodule Purple.Repo do
+  import Ecto.Query
+
   use Ecto.Repo,
     otp_app: :purple,
     adapter: Ecto.Adapters.Postgres
+
+  def paginate(query, p, l) when is_integer(p) and is_integer(l) and p > 0 and l > 0 do
+    query
+    |> offset(^((p - 1) * l))
+    |> limit(^l)
+    |> Purple.Repo.all()
+  end
 end
