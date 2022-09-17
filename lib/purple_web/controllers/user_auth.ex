eab7a295be555419666a77a144129d0d4c6ff000
Nicolas Knoebber <knoebber@gmail.com>
2022-09-17 10:10:14 -0700

    Always set remember me cookie

--- lib/purple_web/controllers/user_auth.ex
+++ lib/purple_web/controllers/user_auth.ex
@@ -32,16 +32,8 @@ defmodule PurpleWeb.UserAuth do
     |> renew_session()
     |> put_session(:user_token, token)
     |> put_session(:live_socket_id, "users_sessions:#{Base.url_encode64(token)}")
-    |> maybe_write_remember_me_cookie(token, params)
+    |> put_resp_cookie(@remember_me_cookie, token, @remember_me_options)
     |> redirect(to: user_return_to || signed_in_path(conn))
-  end
-
-  defp maybe_write_remember_me_cookie(conn, token, %{"remember_me" => "true"}) do
-    put_resp_cookie(conn, @remember_me_cookie, token, @remember_me_options)
-  end
-
-  defp maybe_write_remember_me_cookie(conn, _token, _params) do
-    conn
   end
 
   # This function renews the session ID and erases the whole
