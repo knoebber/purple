5e9e82f634182c2f88c52e8b27112f78db6c3a29
Nicolas Knoebber <knoebber@gmail.com>
2022-12-31 14:50:38 -0700

    Cleanup pdf js

--- assets/js/hooks/pdf.js
+++ assets/js/hooks/pdf.js
@@ -1,43 +1,35 @@
 import pdfJS from 'pdfjs-dist';
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
 
-let isRendering = false;
-let pageNum = 1;
+pdfJS.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 function renderPDF(canvas) {
-  pdfJS.GlobalWorkerOptions.workerSrc = pdfjsWorker;
-  //
-  // Asynchronous download PDF
-  //
-  const loadingTask = pdfJS.getDocument(canvas.dataset.path);
-  (async () => {
-    const pdf = await loadingTask.promise;
-    //
-    // Fetch the first page
-    //
-    let page = await pdf.getPage(1);
-    const viewport = page.getViewport({ scale: 1.5 });
-    // Support HiDPI-screens.
-    const outputScale = window.devicePixelRatio || 1;
+  let pageNum = 1;
 
-    //
-    // Prepare canvas using PDF page dimensions
-    //
-    const context = canvas.getContext('2d');
+  // Handle hidi
+  const outputScale = window.devicePixelRatio || 1;
 
+  function adjustCanvasSize(viewport) {
     canvas.width = Math.floor(viewport.width * outputScale);
     canvas.height = Math.floor(viewport.height * outputScale);
     canvas.style.width = Math.floor(viewport.width) + 'px';
     canvas.style.height = Math.floor(viewport.height) + 'px';
+  }
+
+  const loadingTask = pdfJS.getDocument(canvas.dataset.path);
+  (async () => {
+    const pdf = await loadingTask.promise;
+    let page = await pdf.getPage(1);
+    const viewport = page.getViewport({ scale: 1.5 });
+
+    const context = canvas.getContext('2d');
+    adjustCanvasSize(viewport);
     canvas.classList.add('border', 'border-purple-500', 'rounded');
 
     const transform = outputScale !== 1 
     ? [outputScale, 0, 0, outputScale, 0, 0] 
     : null;
 
-    //
-    // Render PDF page into canvas context
-    //
     let renderContext = {
       canvasContext: context,
       transform,
@@ -64,13 +56,8 @@ function renderPDF(canvas) {
     } 
 
     async function onZoom(e) {
-      console.log('zooming to', parseFloat(e.target.value));
-      console.log(page.getViewport({ scale: parseFloat(e.target.value) }));
       const newViewport = page.getViewport({ scale: parseFloat(e.target.value) });
-      canvas.width = Math.floor(newViewport.width * outputScale);
-      canvas.height = Math.floor(newViewport.height * outputScale);
-      canvas.style.width = Math.floor(newViewport.width) + 'px';
-      canvas.style.height = Math.floor(newViewport.height) + 'px';
+      adjustCanvasSize(newViewport);
       renderContext.viewport = newViewport;
       page = await pdf.getPage(pageNum);
       page.render(renderContext);     
